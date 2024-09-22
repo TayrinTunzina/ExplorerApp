c9dbdd2cb791aa70369aeb18dcd1d2e3ab0d5781
AbuRayhanAbir <72147535+AbuRayhanAbir@users.noreply.github.com>
2024-09-22 09:15:19 +0600

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,52 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+
+
+
 plugins {
-    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.application)
+
+}
+
+android {
+    namespace = "com.example.advancemap"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.advancemap"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.play.services.maps)
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
 }
